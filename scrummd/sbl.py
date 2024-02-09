ecec5a0b928e91f43197f43452c5ad0aefab61e8
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-02-09 21:11:21 +1100

    cli002 Read configuration from file

--- scrummd/sbl.py
+++ scrummd/sbl.py
@@ -1,11 +1,24 @@
 """Display a collection of scrum cards"""
 
+import os
 import argparse
+import tomllib
+from scrummd import const
 
 from scrummd.collection import get_collection
 from scrummd.config import ScrumConfig
 
-config = ScrumConfig()
+
+def load_fs_config() -> ScrumConfig:
+    for filename in const.CONFIG_FILE_NAME:
+        if os.path.exists(filename):
+            config_file = open(filename, "rb")
+            config_settings = tomllib.load(config_file)
+            relevant_settings = config_settings.get("tool", {}).get("scrummd")
+            if relevant_settings:
+                return ScrumConfig(**relevant_settings)
+
+    return ScrumConfig()
 
 
 def entry():
@@ -13,6 +26,8 @@ def entry():
     parser.add_argument("collection")
     parser.description = __doc__
     args = parser.parse_args()
+
+    config = load_fs_config()
 
     collection = get_collection(config, args.collection)
 
--- test/test_sbl.py
+++ test/test_sbl.py
@@ -1,1 +1,83 @@
+import os
+import pytest
+from scrummd import const
 import scrummd.sbl
+import tempfile
+from pathlib import Path
+
+
+@pytest.fixture(scope="function")
+def temp_dir():
+    with tempfile.TemporaryDirectory() as td:
+        while not os.path.exists(td):
+            pass
+        os.chdir(td)
+        yield td
+
+
+@pytest.fixture(scope="function")
+def pyproject_with_config(temp_dir):
+    with open(Path(temp_dir, "pyproject.toml"), "w") as fo:
+        fo.write(PYPROJECT_WITH_CONFIG)
+        fo.flush()
+        yield
+
+
+@pytest.fixture(scope="function")
+def pyproject_without_config(temp_dir):
+    with open(Path(temp_dir, "pyproject.toml"), "w") as fo:
+        fo.write(PYPROJECT_WITHOUT_CONFIG)
+        fo.flush()
+        yield
+
+
+@pytest.fixture(scope="function")
+def scrum_dot_toml(temp_dir):
+    with open(Path(temp_dir, "scrum.toml"), "w") as fo:
+        fo.write(BASIC_TOOL_ONLY)
+        fo.flush()
+        yield
+
+
+PYPROJECT_WITHOUT_CONFIG = """[project]
+name = "scrummd"
+scrum_path = "no_config"
+"""
+
+PYPROJECT_WITH_CONFIG = """[project]
+name = "scrummd"
+
+[tool.scrummd]
+strict = true
+scrum_path = "with_config"
+"""
+
+BASIC_TOOL_ONLY = """
+[tool.scrummd]
+strict = true
+scrum_path = "basic_tool"
+"""
+
+
+def test_no_file(temp_dir):
+    """Test that defaults are used where no config"""
+    config = scrummd.sbl.load_fs_config()
+    assert config.scrum_path == const.DEFAULT_SCRUM_FOLDER_NAME
+
+
+def test_priority(temp_dir, scrum_dot_toml, pyproject_with_config):
+    """Test the priority order of which config works"""
+    config = scrummd.sbl.load_fs_config()
+    assert config.scrum_path == "basic_tool"
+
+
+def test_with_configless_pyproject(temp_dir, pyproject_without_config):
+    """Test that default is used when only pyproject is there and not config in it"""
+    config = scrummd.sbl.load_fs_config()
+    assert config.scrum_path == const.DEFAULT_SCRUM_FOLDER_NAME
+
+
+def test_with_config_pyproject(temp_dir, pyproject_with_config):
+    """Test that the tool works when pyproject is laid out as normal"""
+    config = scrummd.sbl.load_fs_config()
+    assert config.scrum_path == "with_config"
