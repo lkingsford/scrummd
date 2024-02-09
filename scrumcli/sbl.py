2234e1e70c8c368870af3129541d2690da06bf2b
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-02-09 17:23:18 +1100

    cli001 Output index, card to stdout

--- scrumcli/sbl.py
+++ scrumcli/sbl.py
@@ -2,10 +2,23 @@
 
 import argparse
 
+from scrumcli.collection import get_collection
+from scrumcli.config import ScrumConfig
+
+config = ScrumConfig()
+
 
 def entry():
     parser = argparse.ArgumentParser()
+    parser.add_argument("collection")
     parser.description = __doc__
+    args = parser.parse_args()
+
+    collection = get_collection(config, args.collection)
+
+    print("index, summary")
+    for index, card in collection.items():
+        print(f"{index}, {card['summary']}")
 
 
 if __name__ == "__main__":
