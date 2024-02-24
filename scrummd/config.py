7deb8dac2bf474f861923f12d540619b96d631b6
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-02-24 12:44:16 +1100

    cli018 Modify config to use dataclass

--- scrummd/config.py
+++ scrummd/config.py
@@ -1,8 +1,10 @@
 """The ScrumConfig class to configure processing scrum files."""
 
+from dataclasses import dataclass, field
 from scrummd import const
 
 
+@dataclass
 class ScrumConfig:
     """The current configuration for processing scrum files."""
 
@@ -12,25 +14,11 @@ class ScrumConfig:
     strict: bool = False
     """Fail on any error with the scrum folder (such as duplicate index or invalid file)"""
 
-    columns: list[str] = ["index", "summary"]
+    columns: list[str] = field(default_factory=lambda: ["index", "summary"])
     """List of columns to return in output"""
 
     omit_headers: bool = False
     """Omit headers from output"""
 
-    fields: dict[str, str] = {}
+    fields: dict[str, str] = field(default_factory=dict)
     """Fields with limited permitted values and defined order"""
-
-    def __init__(
-        self,
-        scrum_path=scrum_path,
-        strict=strict,
-        columns=columns,
-        omit_headers=omit_headers,
-        fields=fields,
-    ) -> None:
-        self.scrum_path = scrum_path
-        self.strict = strict
-        self.columns = columns
-        self.omit_headers = omit_headers
-        self.fields = fields
