57580a2a53d0be854b84d071689c0f80a474c512
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-02-26 12:25:08 +1100

    cli018 sbl outputs grouped data

--- scrummd/sbl.py
+++ scrummd/sbl.py
@@ -2,7 +2,8 @@
 
 import argparse
 
-from scrummd.collection import get_collection
+from scrummd.collection import Groups, get_collection, group_collection
+from scrummd.config import ScrumConfig
 from scrummd.config_loader import load_fs_config
 
 
@@ -23,14 +24,22 @@ def entry():
         "-b",
         "--bare",
         action="store_true",
-        help="Return bare paths only suitable for scripting. Effectively shorthand for `sbl -o -c _path`.",
+        help="Return bare paths only suitable for scripting. Effectively shorthand for `sbl -H -c _path`.",
     )
     parser.add_argument(
-        "-o",
+        "-H",
         "--omit-headers",
         action="store_true",
         help="Omit headers from output.",
     )
+
+    parser.add_argument(
+        "-g",
+        "--group-by",
+        action="append",
+        help="Group by field in card. Can use multiple group-by arguments to have multiple levels of grouping.",
+    )
+
     parser.description = __doc__
     args = parser.parse_args()
 
@@ -52,9 +61,41 @@ def entry():
     if not omit_headers:
         print(", ".join(columns))
 
-    for card in collection.values():
-        values = [card[col] if (col in card and card[col]) else "" for col in columns]
-        print(", ".join(values))
+    if not args.group_by:
+        for card in collection.values():
+            values = [
+                card[col] if (col in card and card[col]) else "" for col in columns
+            ]
+            print(", ".join(values))
+
+    else:
+        grouped = group_collection(config, collection, args.group_by)
+
+        def output_group(
+            config: ScrumConfig, collection: Groups, group_fields: list[str], level=1
+        ):
+            for group_key, cards in collection.items():
+                if not omit_headers:
+                    print(
+                        f"[" * level
+                        + group_fields[0]
+                        + " = "
+                        + str(group_key)
+                        + "]" * level
+                    )
+
+                if isinstance(cards, dict):
+                    output_group(config, cards, group_fields[1:], level + 1)
+
+                else:
+                    for card in cards:
+                        values = [
+                            card[col] if (col in card and card[col]) else ""
+                            for col in columns
+                        ]
+                        print(", ".join(values))
+
+        output_group(config, grouped, args.group_by)
 
 
 if __name__ == "__main__":
