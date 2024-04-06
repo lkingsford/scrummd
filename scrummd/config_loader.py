7b26c336551803cc5a7f3e38d6ea65e6188a362b
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-04-06 18:22:47 +1100

    cli030 Use tomli on Python 3.10

--- scrummd/config_loader.py
+++ scrummd/config_loader.py
@@ -1,12 +1,36 @@
 """Code for loading the config from the filesystem"""
 
+import sys
 import os
-import tomllib
 from scrummd import const
 from scrummd.config import ScrumConfig
 
+# Need to do this because tomllib wasn't included until Python 3.11
+if sys.version_info[1] < 11:
+    import tomli
 
-def load_fs_config() -> ScrumConfig:
+    LEGACY_TOML = True
+else:
+    import tomllib
+
+    LEGACY_TOML = False
+
+
+def _load_fs_config_legacy() -> ScrumConfig:
+    """Load the config from the filesystem into a ScrumConfig in
+    Python 3.10"""
+    for filename in const.CONFIG_FILE_NAME:
+        if os.path.exists(filename):
+            config_file = open(filename, "rb")
+            config_settings = tomli.load(config_file)
+            relevant_settings = config_settings.get("tool", {}).get("scrummd")
+            if relevant_settings:
+                return ScrumConfig(**relevant_settings)
+
+    return ScrumConfig()
+
+
+def _load_fs_config() -> ScrumConfig:
     """Load the config from the filesystem into a ScrumConfig"""
     for filename in const.CONFIG_FILE_NAME:
         if os.path.exists(filename):
@@ -17,3 +41,6 @@ def load_fs_config() -> ScrumConfig:
                 return ScrumConfig(**relevant_settings)
 
     return ScrumConfig()
+
+
+load_fs_config = _load_fs_config if not LEGACY_TOML else _load_fs_config_legacy
