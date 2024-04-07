bad157b76c48aeb73f7b353cee2b575942d62a66
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-04-07 10:02:07 +1000

    cli030 Adjust to fix typechecker

--- scrummd/config_loader.py
+++ scrummd/config_loader.py
@@ -6,41 +6,24 @@ from scrummd import const
 from scrummd.config import ScrumConfig
 
 # Need to do this because tomllib wasn't included until Python 3.11
-if sys.version_info[1] < 11:
-    import tomli
-
-    LEGACY_TOML = True
-else:
+if sys.version_info >= (3, 11):
     import tomllib
 
-    LEGACY_TOML = False
+    LOAD = tomllib.load
+else:
+    import tomli
+
+    LOAD = tomli.load
 
 
-def _load_fs_config_legacy() -> ScrumConfig:
-    """Load the config from the filesystem into a ScrumConfig in
-    Python 3.10"""
-    for filename in const.CONFIG_FILE_NAME:
-        if os.path.exists(filename):
-            config_file = open(filename, "rb")
-            config_settings = tomli.load(config_file)
-            relevant_settings = config_settings.get("tool", {}).get("scrummd")
-            if relevant_settings:
-                return ScrumConfig(**relevant_settings)
-
-    return ScrumConfig()
-
-
-def _load_fs_config() -> ScrumConfig:
+def load_fs_config() -> ScrumConfig:
     """Load the config from the filesystem into a ScrumConfig"""
     for filename in const.CONFIG_FILE_NAME:
         if os.path.exists(filename):
             config_file = open(filename, "rb")
-            config_settings = tomllib.load(config_file)
+            config_settings = LOAD(config_file)
             relevant_settings = config_settings.get("tool", {}).get("scrummd")
             if relevant_settings:
                 return ScrumConfig(**relevant_settings)
 
     return ScrumConfig()
-
-
-load_fs_config = _load_fs_config if not LEGACY_TOML else _load_fs_config_legacy
