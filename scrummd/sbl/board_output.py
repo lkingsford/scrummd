1436dc543d7fca3bc1c40e36dcf0c4a88046c3cc
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-04-07 23:47:35 +1000

    Multi group progress

--- scrummd/sbl/board_output.py
+++ scrummd/sbl/board_output.py
@@ -12,6 +12,74 @@ def _format_field(field: str, width: int) -> str:
         return stripped_field + " " * (width - len(stripped_field))
 
 
+def _output_last_level_groups(
+    config: ScrumConfig, output_config: OutputConfig, column_width: int, groups: Groups
+):
+    """Output the first level of groups that's actually split
+
+    Args:
+        config (ScrumConfig): Scrum configuratoin
+        output_config (OutputConfig): Output specific config
+        column_width (int): Width in characters of column
+        groups (Groups): Final level of groups to display
+    """
+    text_width = column_width - 1
+
+    # We build the strings for each column, and then display them line by line
+    output_columns: dict[str, list[str]] = {str(key): [] for key in groups.keys()}
+    for group_key, group_value in groups.items():
+        output_list = output_columns[str(group_key)]
+        for card in group_value.collection.values():
+            for field_name in output_config.columns:
+                output_list.append(
+                    _format_field(str(card.get_field(field_name)), text_width)
+                )
+            output_list.append(" " * text_width)
+
+    for line_no in range(len(groups)):
+        value_line = "|"
+        for column in output_columns.values():
+            if len(column) > line_no:
+                value_line += column[line_no] + "|"
+            else:
+                value_line += " " * (column_width - 1) + "|"
+        print(value_line)
+
+
+def _output_group(
+    config: ScrumConfig,
+    output_config: OutputConfig,
+    collection: Groups,
+    group_fields: list[str],
+    column_width: int,
+    level=1,
+):
+    """Internal recursive function to text_grouped_output"""
+    for group_key, cards in collection.items():
+        if len(group_fields) > 2:
+            if not output_config.omit_headers:
+                print(
+                    f"[" * level
+                    + group_fields[0]
+                    + " = "
+                    + str(group_key)
+                    + "]" * level
+                )
+            _output_group(
+                config, output_config, cards.groups, group_fields[1:], level + 1
+            )
+        else:
+            if not output_config.omit_headers:
+                print(
+                    f"[" * level
+                    + group_fields[0]
+                    + " = "
+                    + str(group_key)
+                    + "]" * level
+                )
+            _output_last_level_groups(config, output_config, column_width, cards.groups)
+
+
 def board_grouped_output(
     config: ScrumConfig,
     output_config: OutputConfig,
@@ -27,10 +95,9 @@ def board_grouped_output(
         groups (Groups): Groups to output
     """
     group_count = len(groups)
-    column_width = os.get_terminal_size().columns // group_count
+    column_width = 10  # ; os.get_terminal_size().columns // group_count
 
     # Print headers
-
     header_row = "|"
     for i, group_key in enumerate(groups.keys()):
         if len(str(group_key)) > (column_width - 1):
@@ -47,27 +114,8 @@ def board_grouped_output(
         border_row += "-" * (column_width - 1) + "|"
     print(border_row)
 
-    text_width = column_width - 1
-
-    # We build the strings for each column, and then display them line by line
-    output_columns: dict[str, list[str]] = {str(key): [] for key in groups.keys()}
-    for group_key, group_value in groups.items():
-        output_list = output_columns[str(group_key)]
-        for card in group_value.collection.values():
-            for field_name in output_config.columns:
-                output_list.append(
-                    _format_field(str(card.get_field(field_name)), text_width)
-                )
-            output_list.append(" " * text_width)
-
-    for line_no in range(group_count):
-        value_line = "|"
-        for column in output_columns.values():
-            if len(column) > line_no:
-                value_line += column[line_no] + "|"
-            else:
-                value_line += " " * (column_width - 1) + "|"
-        print(value_line)
+    _output_group(config, output_config, groups, output_config.group_by, column_width)
+    # _output_last_level_groups(config, output_config, column_width, groups)
 
 
 def board_ungrouped_output(
