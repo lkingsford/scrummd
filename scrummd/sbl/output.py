fdb396431deecafb216d686781e2881b6ac6d87b
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-04-07 11:10:47 +1000

    Progress

--- scrummd/sbl/output.py
+++ scrummd/sbl/output.py
@@ -1,9 +1,22 @@
-from typing import Callable
-from scrummd.collection import Groups, Collection
+from typing import Callable, NamedTuple, Optional
+from scrummd.collection import Groups, Collection, SortCriteria
 from scrummd.config import ScrumConfig
 
-SblGroupOutputFunction = Callable[[ScrumConfig, Groups, list[str]], None]
-"""A function that outputs the results of grouped sbl data"""
 
-SblCollectionOutputFunction = Callable[[ScrumConfig, Collection], None]
-"""A function that outputs the results of a collection sbl data"""
+class OutputConfig(NamedTuple):
+    """Common run specific config for all types of output"""
+
+    omit_headings: bool = False
+    group_by: list[str] = []
+    columns: list[str] = ["index", "summary"]
+
+
+SblOutputGroupedFunction = Callable[
+    [ScrumConfig, OutputConfig, Optional[any], Groups], None
+]
+"""Function for outputting groups"""
+
+SblOutputUngroupedFunction = Callable[
+    [ScrumConfig, OutputConfig, Optional[any], Collection], None
+]
+"""Function for outputting bare collection"""
--- scrummd/sbl/text_output.py
+++ scrummd/sbl/text_output.py
@@ -1,10 +1,18 @@
+from typing import Optional
 from scrummd.collection import Groups, Collection
 from scrummd.config import ScrumConfig
+from sbl.output import OutputConfig
 
 
-def group_output(config: ScrumConfig, groups: Groups, group_fields: list[str]):
-    pass
+def text_output(
+    config: ScrumConfig, output_config: OutputConfig, text_config: None, groups: Groups
+) -> None:
+    """Output groups to stdout
 
-
-def collection_output(config: ScrumConfig, collection: Collection):
+    Args:
+        config (ScrumConfig): ScrumConfig
+        output_config (OutputConfig): Output specific config
+        text_config (None): Not used (yet)
+        groups (Groups): Groups to output
+    """
     pass
