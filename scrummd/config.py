00f8a3ef2385a6a8c9af0b9b73f07ba2d44b26f9
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-04-10 23:24:07 +1000

    cli034 Split command specific config into own setions of config file

--- scrummd/config.py
+++ scrummd/config.py
@@ -1,6 +1,7 @@
 """The ScrumConfig class to configure processing scrum files."""
 
 from dataclasses import dataclass, field, fields
+from typing import Optional
 from scrummd import const
 
 
@@ -16,6 +17,36 @@ class CollectionConfig:
 
 
 @dataclass
+class SblConfig:
+    """Configuration specific to sbl"""
+
+    columns: list[str] = field(default_factory=lambda: ["index", "summary"])
+    """List of columns to return in output"""
+    default_group_by: Optional[str] = None
+    """Default group-by for sbl"""
+    omit_headers: bool = False
+    """Omit headers from output"""
+
+
+@dataclass
+class SboardConfig:
+    """Configuration specific to sboard"""
+
+    columns: list[str] = field(default_factory=lambda: ["index", "summary"])
+    """List of columns to return in output"""
+    default_group_by: Optional[str] = None
+    """Default group-by for sboard"""
+
+
+@dataclass
+class ScardConfig:
+    """Configuration specific to scard"""
+
+    reference_format: str = "[$index]"
+    """Fields to show when a card is referenced in a field in `scard`"""
+
+
+@dataclass
 class ScrumConfig(CollectionConfig):
     """The configuration that applies to all cards and ScrumMD"""
 
@@ -25,19 +56,24 @@ class ScrumConfig(CollectionConfig):
     strict: bool = False
     """Fail on any error with the scrum folder (such as duplicate index or invalid file)"""
 
-    columns: list[str] = field(default_factory=lambda: ["index", "summary"])
-    """List of columns to return in output"""
-
-    omit_headers: bool = False
-    """Omit headers from output"""
-
-    scard_reference_format: str = "[$index]"
-    """Fields to show when a card is referenced in a field in `scard`"""
-
     collections: dict[str, CollectionConfig] = field(default_factory=dict)
+    """Embedded collection config"""
+
+    scard: ScardConfig = field(default_factory=ScardConfig)
+
+    sbl: SblConfig = field(default_factory=SblConfig)
+
+    sboard: SboardConfig = field(default_factory=SboardConfig)
 
     def __post_init__(self):
-        """Fix up 'CollectionConfig' which is erroneously a dict after initialising"""
+        """Fix up embedded fields, which default to dicts"""
 
         for key, collection in self.collections.items():
             self.collections[key] = CollectionConfig(**collection)
+
+        if isinstance(self.sbl, dict):
+            self.sbl = SblConfig(**self.sbl)
+        if isinstance(self.scard, dict):
+            self.scard = ScardConfig(**self.scard)
+        if isinstance(self.sboard, dict):
+            self.sboard = SboardConfig(**self.sboard)
