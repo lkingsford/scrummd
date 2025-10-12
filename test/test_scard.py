277a355f683b8e10c1cfeb8f8aed5e096b69324d
Lachlan Kingsford <lachlan@nerdygentleman.com>
2025-10-13 00:07:14 +1100

    cli039 Basic end to end scard test

--- test/test_scard.py
+++ test/test_scard.py
@@ -1,6 +1,7 @@
-from scrummd.scard import format_field
-from scrummd.source_md import Field, FieldNumber, FieldStr
 import pytest
+from fixtures import data_config, TEST_COLLECTION_KEYS, test_collection
+from scrummd.scard import format_field, entry
+from scrummd.source_md import Field, FieldNumber, FieldStr
 
 
 @pytest.mark.parametrize(
@@ -16,3 +17,10 @@ import pytest
 def test_format_field(input_data: Field | list[FieldStr], expected: str):
     """Test that formatting a field produces the expected output"""
     assert format_field(input_data) == expected
+
+
+@pytest.mark.parametrize("card_key", TEST_COLLECTION_KEYS)
+def test_scard_end_to_end(card_key: str, test_collection, data_config):
+    """Basic 'run with default settings on everything' integration test"""
+    # No assertions, because no crash is sufficient
+    entry(["scard", card_key], data_config)
